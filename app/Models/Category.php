39b8198256115866a182d2f2c70e517724422491
mashady <muhammedmashady@gmail.com>
2025-05-13 02:35:09 +0300

    adding category CRUD

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'description', 'parent_id'];
+
     public function parent()
     {
         return $this->belongsTo(Category::class, 'parent_id');
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,6 +17,9 @@ Route::post("/login", [AuthController::class, "login"]);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post("/logout", [AuthController::class, "logout"]);
 });
+
+Route::apiResource('categories', CategoryController::class);
+
 
 /* Route::get('/email/verify', function () {
     return view('auth.verify-email'); // Or whatever view you have
